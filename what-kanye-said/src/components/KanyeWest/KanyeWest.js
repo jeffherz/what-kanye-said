e5312420b89e99852e53868a8e9de98e91b136bd
JJHerz <jeffherz565@gmail.com>
2021-09-21 18:15:32 -0400

    Added some styling and Kanye photo

--- what-kanye-said/src/components/KanyeWest/KanyeWest.js
+++ what-kanye-said/src/components/KanyeWest/KanyeWest.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import "./KanyeWest.css";
 
 export default function KanyeWest() {
 
     return (
-        <div>
-            <h1>This is Where Kanye Goes</h1>
+        <div className="kanye-thought">
+            <img className="kanye-pic" src="kanye-west.jpeg" alt="Kanye West"></img>
         </div>
     );
 }
